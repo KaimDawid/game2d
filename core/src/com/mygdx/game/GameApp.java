dc42a8f4308b9cb20e75f640451304eb4c3676ca
Dawid Kaim <109671153+KaimDawid@users.noreply.github.com>
2022-08-18 19:38:45 +0200

    Initial commit

--- core/src/com/mygdx/game/GameApp.java
+++ core/src/com/mygdx/game/GameApp.java
@@ -235,9 +235,77 @@ public static String playerAttackText2;
 public static BitmapFont playerAttackBMP2;
 	public static String curseTEXT;
 	public static BitmapFont curseBMP;
+Music music_background = new Music() {
+	@Override
+	public void play() {
+
+	}
 
 	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
+	@Override
+	public boolean isPlaying() {
+		return false;
+	}
+
+	@Override
+	public void setLooping(boolean isLooping) {
+
+	}
+
+	@Override
+	public boolean isLooping() {
+		return false;
+	}
+
+	@Override
+	public void setVolume(float volume) {
+
+	}
+
+	@Override
+	public float getVolume() {
+		return 0;
+	}
+
+	@Override
+	public void setPan(float pan, float volume) {
+
+	}
+
+	@Override
+	public void setPosition(float position) {
+
+	}
+
+	@Override
+	public float getPosition() {
+		return 0;
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void setOnCompletionListener(OnCompletionListener listener) {
+
+	}
+};
+	@Override
 	public void create() {
+		music_background = Gdx.audio.newMusic(Gdx.files.internal("bgmusic.mp3"));
+		music_background.setLooping(true);
+		music_background.play();
 		curseTEXT = new String(" ");
 		curseBMP = new BitmapFont();
 playerAttackText2 = new String();
